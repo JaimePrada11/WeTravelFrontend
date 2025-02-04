dd6cbb655179293bca75afac545d4b19bd03f89b
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-04 07:50:18 -0500

    feat: :sparkles: Post Section
    Create post and notifications page

--- pages/feed.js
+++ pages/feed.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var modal = document.getElementById("myModal");
     var openModal = document.getElementById("openModal");
 
-    openModal.onclick = function() {
+    openModal.onclick = function () {
         modal.style.display = "block";
     }
 
     var span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function() {
+    span.onclick = function () {
         modal.style.display = "none";
     }
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
