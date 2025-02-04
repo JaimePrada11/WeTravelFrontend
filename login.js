af27b8c0bae64e4d4c22a7f4872c5d31c59ea401
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-04 18:43:18 -0500

    feat: :sparkles: Integration of the login with the backend
    Integration of the login with the backend and validating the users and their token is generated

---
+++ login.js
@@ -0,0 +1,63 @@
+document.getElementById('loginForm').addEventListener('submit', async function (e) {
+    e.preventDefault(); 
+
+    const userName = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const url = `http://localhost:8080/login?userName=${encodeURIComponent(userName)}&password=${encodeURIComponent(password)}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json' 
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Invalid username or password');
+        }
+
+        const data = await response.json();
+        const token = data.token; 
+        localStorage.setItem('token', token); 
+
+        const userResponse = await fetch('http://localhost:8080/api/users', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (!userResponse.ok) {
+            throw new Error('Failed to fetch user info');
+        }
+
+        const users = await userResponse.json();
+        const user = users.find(user => user.userName === userName);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        const userInfo = {
+            name: user.name,
+            email: user.email,
+            userName: user.userName,
+            biography: user.biography,
+            photo: user.photo,
+            creationDate: user.creationDate,
+            lastLogin: user.lastLogin,
+            editionDate: user.editionDate
+        };
+        
+        localStorage.setItem('user', JSON.stringify(userInfo));
+
+        window.location.href = '/pages/feed.html';
+        alert('Login successful');
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Invalid username or password');
+    }
+});
---
+++ pages/Profile.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('selected'));
+        button.classList.add('selected');
+    });
+});
