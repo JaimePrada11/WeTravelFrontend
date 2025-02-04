f1a2982d16eea9bdc1cc3a50d765ed002f01f9ea
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-04 07:03:18 -0500

    feat: :sparkles: Feed pages

---
+++ pages/feed.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("myModal");
+    var openModal = document.getElementById("openModal");
+
+    openModal.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    var span = document.getElementsByClassName("close")[0];
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
