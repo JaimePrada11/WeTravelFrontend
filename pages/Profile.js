5594183f359ad7de4ed2d851fb782e4a2cb37b61
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-04 19:57:26 -0500

    feat: :sparkles: posts are added to the feed

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,8 +1,20 @@
-const buttons = document.querySelectorAll('.button');
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        buttons.forEach(btn => btn.classList.remove('selected'));
-        button.classList.add('selected');
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => btn.classList.remove('selected'));
+            button.classList.add('selected');
+        });
     });
+
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user) {
+        document.getElementById('name').textContent = user.name;
+        document.getElementById('userName').textContent = `@${user.userName}`;
+        document.getElementById('biography').textContent = user.biography;
+        document.getElementById('photo').src = user.photo;
+    }
 });
--- pages/feed.js
+++ pages/feed.js
@@ -2,19 +2,76 @@ document.addEventListener('DOMContentLoaded', function () {
     var modal = document.getElementById("myModal");
     var openModal = document.getElementById("openModal");
 
-    openModal.onclick = function () {
+    openModal.addEventListener('click', function () {
         modal.style.display = "block";
-    }
+    });
 
     var span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function () {
+    span.addEventListener('click', function () {
         modal.style.display = "none";
-    }
+    });
 
-    window.onclick = function (event) {
+    window.addEventListener('click', function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
+    });
+
+    async function loadPost() {
+        try {
+            const token = localStorage.getItem('token')
+            const response = await fetch('http://localhost:8080/api/combine', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Invalid username or password');
+            }
+            const data = await response.json();
+            data.forEach(post => renderPost(post));
+        } catch (error) {
+            console.error('Error fetching post:', error);
+        }
     }
+
+    function renderPost(data) {
+        const template = document.getElementById('post-template');
+        const clone = template.content.cloneNode(true);
+
+        const postDTO = data.showPostDTO;
+        const user = postDTO.user;
+
+        clone.querySelector('img').src = user.photo;
+        clone.querySelector('h2').textContent = user.name;
+        clone.querySelector('p.text-sm').textContent = `@${user.userName}`;
+        clone.querySelector('#post-content').textContent = postDTO.description;
+        clone.querySelector('#comments-count').textContent = data.commentDTO.length;
+        clone.querySelector('#likes-count').textContent = data.likePostDTO.length;
+
+        const hashtagsContainer = clone.querySelector('#hashtags-container');
+        data.tagDTO.forEach(tag => {
+            const hashtagElement = document.createElement('a');
+            hashtagElement.href = `#${tag.tagContent}`;
+            hashtagElement.textContent = `#${tag.tagContent}`;
+            hashtagElement.classList.add('mr-2', 'text-blue-900');
+            hashtagsContainer.appendChild(hashtagElement);
+        });
+
+        const imagesContainer = clone.querySelector('#post-images-container');
+        data.photoDTOurl.forEach(imageUrl => {
+            const imgElement = document.createElement('img');
+            imgElement.src = imageUrl.url;
+            imgElement.alt = 'Imagen del post';
+            imgElement.classList.add('rounded-2xl', 'w-full', 'max-w-sm', 'object-cover');
+            imagesContainer.appendChild(imgElement);
+        });
+
+        document.getElementById('post-container').appendChild(clone);
+    }
+
+    loadPost();
 });
