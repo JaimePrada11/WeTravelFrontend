cbf624d3f4fcd5d0666ffb0741b5858fa1760845
JaimePrada11 <jandrespradato.11@gmail.com>
2025-02-04 22:17:13 -0500

    feat: :sparkles: Improve feed section

--- pages/feed.js
+++ pages/feed.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function loadPost() {
         try {
-            const token = localStorage.getItem('token')
+            const token = localStorage.getItem('token');
             const response = await fetch('http://localhost:8080/api/combine', {
                 method: 'GET',
                 headers: {
@@ -32,26 +32,27 @@ document.addEventListener('DOMContentLoaded', function () {
                 throw new Error('Invalid username or password');
             }
             const data = await response.json();
-            data.forEach(post => renderPost(post));
+            data.forEach((post, index) => renderPost(post, index));
+
         } catch (error) {
             console.error('Error fetching post:', error);
         }
     }
 
-    function renderPost(data) {
+    function renderPost(data, index) {
         const template = document.getElementById('post-template');
         const clone = template.content.cloneNode(true);
-
+    
         const postDTO = data.showPostDTO;
         const user = postDTO.user;
-
-        clone.querySelector('img').src = user.photo;
-        clone.querySelector('h2').textContent = user.name;
-        clone.querySelector('p.text-sm').textContent = `@${user.userName}`;
+    
+        clone.querySelector('#photo').src = user.photo;
+        clone.querySelector('#name').textContent = user.name;
+        clone.querySelector('#userName').textContent = `@${user.userName}`;
         clone.querySelector('#post-content').textContent = postDTO.description;
         clone.querySelector('#comments-count').textContent = data.commentDTO.length;
         clone.querySelector('#likes-count').textContent = data.likePostDTO.length;
-
+    
         const hashtagsContainer = clone.querySelector('#hashtags-container');
         data.tagDTO.forEach(tag => {
             const hashtagElement = document.createElement('a');
@@ -60,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
             hashtagElement.classList.add('mr-2', 'text-blue-900');
             hashtagsContainer.appendChild(hashtagElement);
         });
-
+    
         const imagesContainer = clone.querySelector('#post-images-container');
         data.photoDTOurl.forEach(imageUrl => {
             const imgElement = document.createElement('img');
@@ -69,9 +70,51 @@ document.addEventListener('DOMContentLoaded', function () {
             imgElement.classList.add('rounded-2xl', 'w-full', 'max-w-sm', 'object-cover');
             imagesContainer.appendChild(imgElement);
         });
-
+    
+        const postcard = clone.querySelector('.postcard');
+        if (index % 2 === 0) {
+            postcard.classList.add('par'); // Post par
+        } else {
+            postcard.classList.add('impar'); // Post impar
+        }
+    
         document.getElementById('post-container').appendChild(clone);
+    }
+    
+    
+
+
+    async function loadTags() {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await fetch('http://localhost:8080/api/tag/tagDTO', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Invalid');
+            }
+            const data = await response.json();
+            const sortedTags = data.sort((a, b) => b.count - a.count);
+            sortedTags.slice(0, 3).forEach(tag => renderTags(tag));
+        } catch (error) {
+            console.error('Error fetching tags:', error);
+        }
+    }
+
+    function renderTags(data) {
+        const template = document.getElementById('tags-template');
+        const clone = template.content.cloneNode(true);
+
+        clone.querySelector('#tagstemplatename').textContent = `#${data.content}`;
+        clone.querySelector('#tagstemplateposts').textContent = `${data.count} publicaciones`;
+
+        document.getElementById('tendencias').appendChild(clone);
     }
 
     loadPost();
+    loadTags();
 });
